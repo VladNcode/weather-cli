b8f90da2a86a362d66aed07f7b3bdc486aecc72c
Vladyslav Nikiforov <vladncode@gmail.com>
2022-03-24 16:05:40 +0200

    Reading and writing settings file

---
+++ services/storage.service.js
@@ -0,0 +1,37 @@
+import { homedir } from 'os';
+import { join } from 'path';
+import { promises } from 'fs';
+
+const filePath = join(homedir(), 'weather-data.json');
+
+const saveKeyValue = async (key, value) => {
+  const data = await readData();
+  data[key] = value;
+
+  await promises.writeFile(filePath, JSON.stringify(data));
+};
+
+const getKeyValue = async key => {
+  const data = await readData();
+  if (data) return data[key];
+
+  return undefined;
+};
+
+const isExist = async path => {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const readData = async () => {
+  if (await isExist(filePath)) {
+    const data = await promises.readFile(filePath, { encoding: 'utf8' });
+    return JSON.parse(data);
+  }
+};
+
+export { saveKeyValue, getKeyValue, readData };
--- weather.js
+++ weather.js
@@ -2,13 +2,19 @@
 
 import { getArgs } from './helpers/args.js';
 import { printError, printHelp, printSuccess } from './services/log.service.js';
+import { getKeyValue, saveKeyValue } from './services/storage.service.js';
+
+const saveData = async (type, data) => {
+  try {
+    await saveKeyValue(`${type === 'token' ? 'token' : 'town'}`, data);
+    printSuccess(`${type === 'token' ? 'Token' : 'Town'} was saved!`);
+  } catch (e) {
+    printError(e.message);
+  }
+};
 
 const initCLI = function () {
   const args = getArgs();
-  console.log(args);
-
-  printError();
-  printSuccess();
 
   if (args.h) {
     // Display help
@@ -17,10 +23,12 @@ const initCLI = function () {
 
   if (args.s) {
     // Save town
+    return saveData('town', args.s);
   }
 
   if (args.t) {
     // Save token
+    return saveData('token', args.t);
   }
 
   // Show weather
